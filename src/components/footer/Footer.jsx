781891ecc4a5c34cb3e53d3f6add8164701af484
ValSpp <muhammadnovals334@gmail.com>
2024-02-25 09:20:18 +0700

    feat: populer movie and search engine

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+                <ul className="menuItems">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="infoText">
+                    Palpy - Movies
+                    Copyright by : Muhammad Noval Supriyadi 
+                </div>
+                <div className="socialIcons">
+                    <a className="icon">
+                        <FaFacebookF />
+                    </a>
+                    <a href="https://www.instagram.com/nopalspryd" target="blank" className="icon">
+                        <FaInstagram />
+                    </a>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
